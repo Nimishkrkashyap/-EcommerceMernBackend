8fe624f146510013f2aaf42ac8506debf828b693
Nimish Kumar <67366262+Nimishkrkashyap@users.noreply.github.com>
2024-08-08 01:48:27 -0700

    User model added

---
+++ model/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Name cannot exceed 30 characters"],
+        minLength: [4, "Name should have more than 4 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter a valid email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your name"],
+        minLength: [8, "Password should have more than 8 characters"],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role:{
+        type:String,
+        default:"user"
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+})
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = User;
